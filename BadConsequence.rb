dca357bc6f88571a2668ace791fb21453bda2b0b
robertohueso <robertohueso96@gmail.com>
2016-03-01 22:52:53 +0100

    Añade clase BadConsequence
    Falta implementar inicializadores. Se realizará en la siguiente sesión

---
+++ BadConsequence.rb
@@ -0,0 +1,26 @@
+class BadConsequence
+   #FIXME Implementar Inicializador! (En la siguiente sesion)
+
+   #Getters basicos
+   def getText
+      return @text
+   end
+
+   def getLevels
+      return @levels
+   end
+
+   def getNVisible
+      return @nVisibleTreasures
+   end
+
+   def getNHidden
+      return @nHiddenTreasures
+   end
+
+   #Conversor a texto
+   def to_s
+      return @text
+   end
+
+end
