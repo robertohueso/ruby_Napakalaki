9b56844917e4f1529c2f9e7e59cd9b357eefc1f7
robertohueso <robertohueso96@gmail.com>
2016-04-02 19:14:10 +0200

    Añade clase Treasure

---
+++ Treasure.rb
@@ -0,0 +1,19 @@
+class Treasure
+   #Constructor
+   def initialize(n, bonus, t)
+      @name = n
+      @bonus = bonus
+      @type = t
+   end
+
+   #Metodos publicos
+   public
+   def getName()
+   end
+
+   def getBonus()
+   end
+
+   def getType()
+   end
+end
