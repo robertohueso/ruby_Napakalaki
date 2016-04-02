9070520a6a11594e9798c33e5d7ea7751f620f74
robertohueso <robertohueso96@gmail.com>
2016-04-02 18:57:23 +0200

    Añade clase Player

---
+++ Player.rb
@@ -0,0 +1,82 @@
+class Player
+   MAXLEVEL = 10
+   #Constructor
+   def initialize(name)
+      @name = name
+      @level = 1
+      @dead = true
+      @hiddenTreasures = []
+      @visibleTreasures = []
+      @pendingBadConsequence = nil
+   end
+
+   #Metodos privados
+   private
+   def bringToLife()
+   end
+
+   def getCombatLevel()
+   end
+
+   def incrementLevels(l)
+   end
+
+   def decrementLevels(l)
+   end
+
+   def setPendingBadConsequence(b)
+   end
+
+   def applyPrize(m)
+   end
+
+   def applyBadConsequence(m)
+   end
+
+   def canMakeTreasureVisible(t)
+   end
+
+   def howManyVisibleTreasures(tKind)
+   end
+
+   def dieIfNoTreasures()
+   end
+
+   #Metodos publicos
+   public
+   def getName()
+   end
+
+   def isDead()
+   end
+
+   def getHiddenTreasures()
+   end
+
+   def getVisibleTreasures()
+   end
+
+   def combat(m)
+   end
+
+   def makeTreasureVisible(t)
+   end
+
+   def discardVisibleTreasure(t)
+   end
+
+   def discardHiddenTreasure(t)
+   end
+
+   def validState()
+   end
+
+   def initTreasures()
+   end
+
+   def getLevels()
+   end
+
+   def discardAllTreasures()
+   end
+end
