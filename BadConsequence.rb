14bfdd162d3ea3c8b3c2cfcaa88613a791408460
robertohueso <robertohueso96@gmail.com>
2016-04-04 18:10:51 +0200

    Usa MAXLEVEL en BadConsequence

--- BadConsequence.rb
+++ BadConsequence.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 
-require_relative 'TreasureKind'
+require_relative "TreasureKind"
+require_relative "Player"
 
 class BadConsequence
    MAXTREASURES = 10
@@ -24,7 +25,7 @@ class BadConsequence
    end
 
    def self.newDeath(aText)
-      new(aText, 10, MAXTREASURES, MAXTREASURES, [], [])
+      new(aText, Player.MAXLEVEL, MAXTREASURES, MAXTREASURES, [], [])
    end
 
    #Metodos publicos
