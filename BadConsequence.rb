18533dd88a209ef26db35a0761b3b79f32270fff
robertohueso <robertohueso96@gmail.com>
2016-04-21 20:14:58 +0200

    Implementa adjustToFit Termina sesión 2

--- BadConsequence.rb
+++ BadConsequence.rb
@@ -66,6 +66,43 @@ class BadConsequence
    end
 
    def adjustToFitTreasureList(v, h)
+     visibleKind = []
+     hiddenKind = []
+     visibleToLose = []
+     hiddenToLose = []
+     nVisible = @nVisibleTreasures
+     nHidden = @nHiddenTreasures
+
+     v.each{ |treasure|
+       visibleKind << treasure.getType
+     }
+     h.each{ |treasure|
+       hiddenKind << treasure.getType
+     }
+
+     if(@specificVisibleTreasures.empty? && @specificHiddenTreasures.empty?)
+       #Ajusta la cantidad de tesoros perdidos.
+       if nVisible > visibleKind.size
+         nVisible = visibleKind.size
+       end
+       if nHidden > hiddenKind.size
+         nHidden = hiddenKind.size
+       end
+       return BadConsequence.newLevelNumberOfTreasures(@text, @levels, nVisible, nHidden)
+     else
+       #Ajusta los tesoros especificos.
+       @specificVisibleTreasures.each{ |kind|
+         if visibleToLose.count(kind) < visibleKind.count(kind)
+           visibleToLose << kind
+         end
+       }
+       @specificHiddenTreasures.each{ |kind|
+         if hiddenToLose.count(kind) < hiddenKind.count(kind)
+           hiddenToLose << kind
+         end
+       }
+       return BadConsequence.newLevelSpecificTreasures(@text, @levels, visibleToLose, hiddenToLose)
+     end
    end
 
    #Conversor a texto
