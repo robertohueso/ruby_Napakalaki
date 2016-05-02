c463cc16362327b4e9d754467ac44a2c5c2fd2e8
robertohueso <robertohueso96@gmail.com>
2016-05-02 16:42:58 +0200

    Añade nuevas clases

---
+++ Cultist.rb
@@ -0,0 +1,12 @@
+#encoding: utf-8
+
+class Cultist
+  def initialize(name, gainedLevels)
+    @name = name
+    @gainedLevels = gainedLevels
+  end
+
+  def getGainedlevels()
+    @gainedLevels
+  end
+end
---
+++ CultistPlayer.rb
@@ -0,0 +1,40 @@
+#encoding: utf-8
+
+require_relative "Cultist"
+require_relative "Player"
+
+class CultistPlayer < Player
+
+  @@totalCultistPlayers = 0
+
+  def initialize(p, c)
+    @@totalCultistPlayers += 1
+    @myCultistCard = c
+    #FIXME Implementar
+
+  end
+
+  def CultistPlayer.getTotalCultistPlayers()
+    @@totalCultistPlayers
+  end
+
+  protected
+
+  def getCombatLevel
+  end
+
+  def getOponentLevel(m)
+  end
+
+  def shouldConvert()
+  end
+
+  private
+
+  def giveMeATreasure()
+  end
+
+  def canYouGiveMeATreasure()
+  end
+
+end
