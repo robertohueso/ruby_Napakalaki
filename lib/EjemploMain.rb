8bb82ffd69c85e7528fbe3c136c162dbc39b3370
robertohueso <robertohueso96@gmail.com>
2016-05-10 16:42:25 +0200

    Cambia tester

--- lib/EjemploMain.rb
+++ lib/EjemploMain.rb
@@ -2,27 +2,27 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-require "./Napakalaki.rb"
-require "./lib/GameTester.rb"
+require_relative "Napakalaki"
+require_relative "GameTester"
 
 module NapakalakiGame
 
   class EjemploMain
-
+   
       def prueba
-
+        
        test = Test::GameTester.instance
-
+     
        game = Napakalaki.instance
-
+   
        #Se prueba el juego con 2 jugadores
-
+   
        test.play(game, 2);
-
+       
       end
-
+      
   end
-
+  
     e = EjemploMain.new
     e.prueba()
 
