72539c7ec25a7f4a38465a1fa16f8acb18cba5e9
robertohueso <robertohueso96@gmail.com>
2016-05-10 16:27:28 +0200

    Elimina constructores BadConsequence

--- BadConsequence.rb
+++ BadConsequence.rb
@@ -14,24 +14,8 @@ class BadConsequence
       @levels = someLevels
    end
 
-   def self.newLevelNumberOfTreasures(aText, someLevels, someVisibleTreasures, someHiddenTreasures)
-      new(aText, someLevels, someVisibleTreasures, someHiddenTreasures, [], [])
-   end
-
-   def self.newLevelSpecificTreasures(aText, someLevels, someSpecificVisibleTreasures, someSpecificHiddenTreasures)
-      new(aText, someLevels, 0, 0, someSpecificVisibleTreasures, someSpecificHiddenTreasures)
-   end
-
-   def self.newDeath(aText)
-      new(aText, Player::MAXLEVEL, MAXTREASURES, MAXTREASURES, [], [])
-   end
-
    #Metodos publicos
    public
-   def isEmpty
-      return @nVisibleTreasures == 0 && @nHiddenTreasures == 0 &&
-             @specificHiddenTreasures.empty? && @specificVisibleTreasures.empty?
-   end
 
    def getLevels
       return @levels
