e0282253ef2d6a5fd82ff3169b959a5f5d321c55
robertohueso <robertohueso96@gmail.com>
2016-05-11 11:24:56 +0200

    Añade metodos abstractos

--- BadConsequence.rb
+++ BadConsequence.rb
@@ -18,6 +18,21 @@ class BadConsequence
       return @levels
    end
 
+   def substractVisibleTreasure(t)
+    raise NotImplementedError.new
+   end
+
+   def substractHiddenTreasure(t)
+    raise NotImplementedError.new
+   end
+
+   def isEmpty
+    raise NotImplementedError.new
+   end
+
+   def adjustToFitTreasureList(v, h)
+     raise NotImplementedError.new
+   end
    #Conversor a texto
    def to_s
       return @text
